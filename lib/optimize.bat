3be4eb1c257ceb70a1989dcb6639da4fc7ea9a31
g.gogolev <g.gogolev@labirint-t.ru>
2023-01-25 14:53:40 +0300

    Оптимизировал скрипт подготовки библиотек

--- lib/optimize.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-copy /Y bpi-lib_out\Excel.lib
-upx_32 -9 Release-Win32\ExcelAPI.dll
-upx_32 -9t Release-Win32\ExcelAPI.dll
-Pause
---
+++ lib/prepare.bat
@@ -0,0 +1,21 @@
+:: Файл подготовки библиотек, чтобы все и сразу
+
+@echo off
+
+:: Перемещение lib
+copy /Y bpi-lib_out\Excel.lib
+
+:: Сжатие dll
+if exist upx_32 (
+upx_32 -9 Release-Win32\ExcelAPI.dll
+upx_32 -9t Release-Win32\ExcelAPI.dll
+) 
+if exist upx (
+upx -9 Release-Win32\ExcelAPI.dll
+upx -9t Release-Win32\ExcelAPI.dll
+) else (
+echo UPX not found! Compress is not done!
+)
+
+
+Pause
