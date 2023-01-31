ec88d3c665c26b5c4eee0a7341c89a1ecfeedc57
g.gogolev <g.gogolev@labirint-t.ru>
2023-01-31 22:22:31 +0300

    Порядок в исключениях

--- src/Exceptions/uTExcelExceptions.cpp
+++ src/Exceptions/uTExcelExceptions.cpp
@@ -9,3 +9,20 @@
 
 #pragma package(smart_init)
 
+//---------------------------------------------------------------------------
+ExcelException::ExcelException(const String& message)
+    : Exception(message)
+{}
+
+ExcelException::ExcelException(const char* message)
+    : Exception(message)
+{}
+
+ExcelException::ExcelException(const String& classWhere, const String& method, const String& message)
+    : Exception(classWhere + "::" + method + ": " + message)
+{}
+
+ExcelException::ExcelException(const char* classWhere, const char* method, const char* message)
+    : Exception(String(classWhere) + "::" + String(method) + ": " + String(message))
+{}
+
